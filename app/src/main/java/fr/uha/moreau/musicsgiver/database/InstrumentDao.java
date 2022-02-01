5fe74d44e5897d8ab491ffb9148cabcc0679fbe8
SimonMoreau10 <simon.moreau4@gmail.com>
2022-02-01 22:55:50 +0100

    InstrumentsListViewModel.java ajouté, et images pour la navigation

--- app/src/main/java/fr/uha/moreau/musicsgiver/database/InstrumentDao.java
+++ app/src/main/java/fr/uha/moreau/musicsgiver/database/InstrumentDao.java
@@ -1,8 +1,11 @@
 package fr.uha.moreau.musicsgiver.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Transaction;
 
 import fr.uha.moreau.musicsgiver.model.Instrument;
 
@@ -11,4 +14,7 @@ public interface InstrumentDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     long upsert(Instrument instrument);
+
+    @Query("Select * from instruments where id = :id")
+    public LiveData<Instrument> getById(long id);
 }
---
+++ app/src/main/java/fr/uha/moreau/musicsgiver/ui/instruments/InstrumentsListViewModel.java
@@ -0,0 +1,57 @@
+package fr.uha.moreau.musicsgiver.ui.instruments;
+
+import android.graphics.Bitmap;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+import fr.uha.moreau.musicsgiver.database.InstrumentDao;
+import fr.uha.moreau.musicsgiver.model.ClasseDInstrument;
+import fr.uha.moreau.musicsgiver.model.Instrument;
+
+public class InstrumentsListViewModel extends ViewModel {
+    private InstrumentDao instrumentDao;
+    private LiveData<Instrument> instrument;
+    private MutableLiveData<Long> id = new MutableLiveData<>();
+    private LiveData<ClasseDInstrument> classeDInstrument;
+    private LiveData<String> nom;
+
+    public void setInstrumentDao(InstrumentDao instrumentDao) {
+        this.instrumentDao = instrumentDao;
+        this.instrument = Transformations.switchMap(id, instrumentDao::getById);
+        this.classeDInstrument = Transformations.map(instrument, Instrument::getClasse);
+        this.nom = Transformations.map(instrument, Instrument::getNom);
+    }
+
+    public LiveData<Instrument> getInstrument() {
+        return instrument;
+    }
+    public LiveData<ClasseDInstrument> getClasseDInstrument() {
+        return classeDInstrument;
+    }
+    public LiveData<String> getNom() {
+        return nom;
+    }
+    public MutableLiveData<Long> getId() {
+        return id;
+    }
+
+    public void setId(MutableLiveData<Long> id) {
+        this.id = id;
+    }
+    public void setInstrument(LiveData<Instrument> instrument) {
+        this.instrument = instrument;
+    }
+    public void setClasseDInstrument(LiveData<ClasseDInstrument> classeDInstrument) {
+        this.classeDInstrument = classeDInstrument;
+    }
+    public void setNom(LiveData<String> nom) {
+        this.nom = nom;
+    }
+
+}
